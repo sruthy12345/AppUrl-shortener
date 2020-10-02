5ee040211c76e149304348c763fdd86f91f8e369
Sruthy <sruthy100ny@gmail.com>
2020-10-02 22:08:43 +0530

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app= express();
 
 const bodyParser= require('body-parser')
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/UrlApp', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/UrlApp',{ useNewUrlParser: true } );
 
 const {UrlModel} = require('./models/urlshort');
 app.use(express.static('public'));
@@ -11,6 +11,7 @@ app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended:true}))
 app.get('/', function(req,res){
    let allUrl=UrlModel.find(function(err,result){
+     if (err) throw err;
     res.render('home', {
         urlResult : result
     })
@@ -26,9 +27,7 @@ app.post('/create', function (req,res) {
         shortUrl : generateUrl()
     })
     urlShort.save(function(err,data){
-       
         if (err) throw err;
-        
          res.redirect('/');
     })
   });
@@ -36,19 +35,16 @@ app.post('/create', function (req,res) {
   app.get('/:urlId', function (req, res) {
     UrlModel.findOne({ shortUrl: req.params.urlId }, function (err, data) {
         if (err) throw err;
-
-        UrlModel.findByIdAndUpdate({ _id: data.id }, { $inc: { clickCount: 1 } }, function (err, updatedData) {
-            if (err) throw err;
             res.redirect(data.longUrl)
         })
 
 
     })
-})
+
 
 app.get('/delete/:id',function(req,res){
     UrlModel.findByIdAndDelete({_id:req.params.id},function(err,deleteData){
-        if(err) throw err;
+        if (err) throw err;
         res.redirect('/')
     })
 })
@@ -61,4 +57,4 @@ app.listen(8000, function() {
       var rndResult= Math.floor(Math.random()*100000);
       console.log(rndResult)
       return rndResult;
-  }
+  }  
